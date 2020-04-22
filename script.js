774ac71210aee4bb9b3a828007bf8229124ac879
hudaman0621 <mrhu828@gmail.com>
2020-04-22 11:10:59 -0400

    added Hello watchkit

--- script.js
+++ script.js
@@ -42,46 +42,74 @@ link2.href = "#";
 link2.style.color = "white";
 topNav.appendChild(link2);
 
-
 container.appendChild(header);
-const footer = document.createElement("footer");
-footer.style.color = "white";
-footer.style.padding = "0 20px";
-footer.textContent = "test";
-container.appendChild(footer);
 
 //curse of the current reviews 
 
 const container2 = document.getElementById('container2');
 document.body.style.fontFamily = 'sans-serif';
-container2.style.padding = "0 20px";
+container2.style.padding = "20px";
 container2.style.marginLeft = '20px';
-
+container2.style.marginTop = "20px";
 
 const header2 = document.createElement('header2');
-header2.style.right="20px"; //come back to get the indentation
 header2.style.color = "#42464c";
 header2.style.backgroundColor = '#d5d6d9';
-header2.style.padding = "0 20px";
-header2.style.margin = "200px 1000px";
-header2.style.border = "10px";
-header2.style.borderColor = "10px";
 
 
 const heading2 = document.createElement('h2');
 container2.style.backgroundColor = "#d5d6d9";
 heading2.textContent = 'Curse of the Current Reviews';
-// heading2.style.paddingRight = "20px";
-// heading2.style.margin = "20px";
 heading2.style.backgroundColor = "#d5d6d9";
 header2.appendChild(heading2);
 
 const paragraph1 = document.createElement('para1');
-// paragraph1.style.display = "flex";
-paragraph1.textContent = "when you want to buy something from Apple, expect to pay a high price"
-paragraph1.style.width = "200px";
-paragraph1.style.padding = "10px";
+paragraph1.style.display = "flex";
+paragraph1.textContent = "when you want to buy something from Apple, expect to pay a high price. when you want to buy something from Apple, expect to pay a high price when you want to buy something from Apple, expect to pay a high price when you want to buy something from Apple, expect to pay a high price when you want to buy something from Apple, expect to pay a high price"
+paragraph1.style.width = "100%";
+paragraph1.style.padding = "1px";
+paragraph1.style.marginLeft = "0px";
+
 header2.appendChild(paragraph1);
-
-
 container2.appendChild(header2);
+
+//Hello WatchKit
+const container3 = document.getElementById('container3');
+document.body.style.fontFamily = "sans-serif";
+container3.style.padding = "20px";
+container3.style.marginLeft = "20px";
+container3.style.marginTop = "20px";
+
+const header3 = document.createElement('header3');
+header3.style.color = "#42464c";
+header3.style.backgroundColor = '#d5d6d9';
+
+const heading3 = document.createElement('h2');
+container3.style.backgroundColor = "brown";
+heading3.textContent = "Hello WatchK";
+heading3.style.backgroundColor = "#d5d6d9";
+header3.appendChild(heading3);
+
+// const heading3 = document.createElement('h2');
+// container3.style.backgroundColor = "#d5d6d9";
+// heading3.textContent = 'Curse of the Current Reviews';
+// heading3.style.backgroundColor = "#d5d6d9";
+// header3.appendChild(heading3);
+
+const paragraph2 = document.createElement('para2');
+paragraph2.style.display = "flex";
+paragraph2.textContent = "APPLE and orange";
+paragraph2.style.width = "100%";
+paragraph2.style.padding = "1px";
+paragraph2.style.marginLeft = "0px";
+
+header3.appendChild(paragraph2);
+container3.appendChild(header3);
+
+
+
+// const footer = document.createElement("footer");
+// footer.style.color = "white";
+// footer.style.padding = "0 20px";
+// footer.textContent = "test";
+// container.appendChild(footer);
